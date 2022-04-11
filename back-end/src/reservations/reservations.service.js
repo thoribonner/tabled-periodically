057d49c2280a932ae8402aec52c060eef96944d7
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-04-11 11:17:42 -0400

    initial setup

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -10,36 +10,9 @@ function list() {
   return knex(tn).select("*").orderBy("reservation_time");
 }
 
-/**
- * Return all reservations for a given date, where the status is not finished or cancelled, ordered by
- * reservation time.
- * @param reservation_date - the date of the reservation
- * @returns An array of reservation objects matching date
- */
- function listByDate(reservation_date) {
-  return knex(res)
-    .select("*")
-    .where({ reservation_date })
-    .whereNot({ status: "finished" })
-    .whereNot({ status: "cancelled" })
-    .orderBy("reservation_time");
-}
 
-/**
- * It takes a reservation object, inserts it into the database, and returns the created reservation
- * @param reservation - the reservation object that we want to create
- * @returns The created reservation.
- */
- function create(reservation) {
-  return knex(res)
-    .insert(reservation, "*")
-    .returning("*")
-    .then((createdRes) => createdRes[0]);
-}
 
 
 module.exports = {
   list,
-  listByDate,
-  create
 }
